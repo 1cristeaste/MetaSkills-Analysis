640176a666df8850f5b70919acec975b609f31cb
1cristeaste <stcristea@gmail.com>
2020-12-30 00:19:04 -0500

    Bug Fix

--- DailyPlanningYearofStudy.py
+++ DailyPlanningYearofStudy.py
@@ -10,6 +10,7 @@ from statsmodels.graphics.gofplots import qqplot_2samples
 import statsmodels.api as sm
 
 if __name__ == "__main__":
+
     def join_w_grades(grades_filepath: str,
                       metaskills_resp_filepath: str) -> pd.DataFrame:
         df_grades = pd.read_csv(grades_filepath)
@@ -18,13 +19,12 @@ if __name__ == "__main__":
                       right_on="SIS Login ID")
         return df
 
-
     Year_of_Study = "Q626"
     Daily_Planning = "Q58"
 
     df = join_w_grades("Data/cleaned_grades.csv",
                        "Data/cleaned_metaskills_1.csv")
-    df = df.merge(df, pd.read_csv("Data/cleaned_metaskills_3.csv"),
+    df = pd.merge(df, pd.read_csv("Data/cleaned_metaskills_3.csv"),
                   left_on="Q62_2", right_on="Q10_2")
 
     Liked_Daily_Planning = df.loc[(df[Daily_Planning] == "Strongly agree") |
@@ -38,7 +38,10 @@ if __name__ == "__main__":
         (df[Daily_Planning] == "Mostly disagree") |
         (df[Daily_Planning] == "Somewhat disagree")]
 
-    fig = plt.figure()
+    fig = df.plot.bar(x=Year_of_Study)
+
+    '''fig = plt.figure()
     fig.bar(Liked_Daily_Planning[Year_of_Study])
     fig.bar(Disliked_Daily_Planning[Year_of_Study] + 0.25)
-    fig.savefig("Plots/YearofStudy DailyPlanning")
+    fig.savefig("Plots/YearofStudy DailyPlanning")'''
+
